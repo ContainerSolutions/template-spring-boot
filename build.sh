f2fd8dfc545a34984dbf979b53ce54f2d6798f11
Frank Scholten <frank@frankscholten.nl>
2015-03-03 16:48:28 +0100

    Build container now creates binaries tarball and run container adds it.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build --no-cache -t template-spring-boot-build -f Dockerfile.build .
-docker run -v /root/.m2/repository:/root/.m2/repository template-spring-boot-build > app.jar
+docker run -v /root/.m2/repository:/root/.m2/repository template-spring-boot-build > binaries.tar.gz
 
 docker build --no-cache -t template-spring-boot -f Dockerfile.run .
 docker run -p 80:8080 template-spring-boot
